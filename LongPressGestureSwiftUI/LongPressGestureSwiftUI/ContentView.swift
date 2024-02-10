0c3ea4678047a42aa3edbd01d61145ada1ae179b
Michael Parekh <mparekh4@illinois.edu>
2024-02-10 14:20:03 -0600

    add longpressgesture modifier

--- LongPressGestureSwiftUI/LongPressGestureSwiftUI/ContentView.swift
+++ LongPressGestureSwiftUI/LongPressGestureSwiftUI/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                .fill(isSuccess ? Color.green : Color.blue)
+                // Determines the progress of the blue bar.
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("CLICK HERE")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) { (isPressing) in
+                        // Called from start of press => minimumDuration.
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                // Move the blue bar to the right as we are pressing.
+                                isComplete = true
+                            }
+                        } else {
+                            // If we stop pressing the button, then move the bar back.
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                // Run the DispatchQueue with delay because 'perform' may run at the same time.
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    } perform: {
+                        // Called at the minimumDuration.
+                        withAnimation(.easeInOut) {
+                            // Change the color of the bar to green when successful.
+                            isSuccess = true
+                        }
+                    }
+
+                Text("RESET")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
         }
-        .padding()
     }
 }
 
