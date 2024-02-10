13c689997dea6a28fa5a2e766df88f7f1cc2507e
Michael Parekh <mparekh4@illinois.edu>
2024-02-09 19:12:29 -0600

    init project

---
+++ LongPressGestureSwiftUI/LongPressGestureSwiftUI/LongPressGestureSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongPressGestureSwiftUIApp.swift
+//  LongPressGestureSwiftUI
+//
+//  Created by Michael Parekh on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LongPressGestureSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
